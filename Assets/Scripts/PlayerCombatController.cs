b31c7964c7e30ed5d043fab60f5917fc00b90c5c
miloszkryzia1 <milosz.kryzia@gmail.com>
2023-04-17 18:26:57 -0700

    add PlayerCombatController script

---
+++ Assets/Scripts/PlayerCombatController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombatController : MonoBehaviour
+{
+    [SerializeField] public float meleeCooldown = 0.5f;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Melee()
+    {
+        
+    }
+}
