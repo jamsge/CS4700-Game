b8fb1e6940ad074970fd4142ebe222de5fe55b10
miloszkryzia1 <milosz.kryzia@gmail.com>
2023-04-19 11:42:05 -0700

    add weapon switching and awake method to keep WeaponManager in all scenes

--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -1,34 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class WeaponManager : MonoBehaviour
 {
+    public static WeaponManager instance;
     public Weapon currentWeapon; 
     //public float maxAmmo = 0; //moved to weapon class
     //public float currentAmmo = 0;
     public Weapon[] weapons;
     public GameObject player;
 
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    
     void Start()
     {
         weapons = new Weapon[5];
         player = GameObject.Find("Player");
         weapons[0] = new Flamethrower();
         currentWeapon = weapons[0]; 
+        print(Array.IndexOf(weapons, currentWeapon));
     }
 
     void Update()
     {
-        if (Input.GetAxis("Mouse ScrollWheel") >= 0.1)
-        {
-            SwitchWeapon();
-        }   
         player = GameObject.Find("Player");
+        float input = Input.GetAxis("Mouse ScrollWheel");
+        if (input != 0)
+        {
+            SwitchWeapon(input);
+        }   
     }
 
-    void SwitchWeapon()
+    void SwitchWeapon(float input)
     {
-        print("switching"); //debug
+        int currentWeaponIndex = Array.IndexOf(weapons, currentWeapon);
+        if (input > 0)
+        {
+            currentWeaponIndex -= 1;
+            if (currentWeaponIndex >= 0)
+            {
+                currentWeapon = weapons[currentWeaponIndex];
+            }
+        }
+        else if (input < 0)
+        {
+            currentWeaponIndex += 1;
+            if (currentWeaponIndex <= 4)
+            {
+                currentWeapon = weapons[currentWeaponIndex];
+            }
+        }
     }
 }
