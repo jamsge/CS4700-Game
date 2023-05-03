39a71e8d3edf8508ffec6aa3a52d4161ea42668c
miloszkryzia1 <milosz.kryzia@gmail.com>
2023-05-02 22:12:04 -0700

    add boss first phase attack

--- Assets/Scripts/Boss/FirstPhase.cs
+++ Assets/Scripts/Boss/FirstPhase.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class FirstPhase : MonoBehaviour
 {   
     public GameObject player;
+    public PlayerData playerData;
     public float attackCooldown; 
+    public float attackPrepareTime;
     public float attackHeight;
     public float attackRange;
     public float attackKnockbackStrength;
+    public int attackDamage;
+    public float playerMovementDisableDuration; //needs to be more than 0, otherwise knockback doesn't work
     bool attacking = false;
     Transform t;
     Rigidbody2D rb;
@@ -25,6 +29,8 @@ public class FirstPhase : MonoBehaviour
         {
             StartCoroutine(Attack());
         }
+
+        //debug
     }
 
     IEnumerator Attack()
@@ -32,15 +38,23 @@ public class FirstPhase : MonoBehaviour
         attacking = true;
         //add attack
         rb.constraints = RigidbodyConstraints2D.FreezeAll; //stop movement when attacking
+        Vector3 attackDirection = player.transform.position - t.position; //calculate attack direction
+        Debug.DrawRay(t.position, attackDirection.normalized * attackRange, Color.red, attackPrepareTime); //debug
 
-        Vector3 attackDirection = player.transform.position - t.position; //FIX ATTACK DIRECTION //ADD DEBUG DRAWRAYS
-        //attackDirection.Normalize();
-        RaycastHit2D hit = Physics2D.CircleCast(t.position, attackHeight, t.TransformDirection(Vector3.right), attackRange, 1 >> 6); //6 is player layer
+        yield return new WaitForSeconds(attackPrepareTime);
+        RaycastHit2D hit = Physics2D.CircleCast(t.position, attackHeight, attackDirection.normalized, attackRange, 1 << 6); //6 is player layer
+
         if (hit)
         {
-            Rigidbody2D playerRB = hit.collider.gameObject.GetComponent<Rigidbody2D>();
-            playerRB.velocity = t.TransformDirection(Vector3.left) * attackKnockbackStrength;
+            playerData.takeHit(attackDamage); //damage player
+            Rigidbody2D playerRB = hit.collider.gameObject.GetComponent<Rigidbody2D>(); //get player's rigidbody
+            print("BOSS HIT PLAYER"); //debug
+            player.GetComponent<PlayerMovementController>().enabled = false; //disable player movement during knockback
+            playerRB.velocity = (Vector2)t.TransformDirection(Vector3.right) * attackKnockbackStrength; //knock back player
+            yield return new WaitForSeconds(playerMovementDisableDuration); //
+            player.GetComponent<PlayerMovementController>().enabled = true; //enable player movement again
         }
+
         yield return new WaitForSeconds(1);
         rb.constraints = RigidbodyConstraints2D.None; //resume movement
 
@@ -48,8 +62,8 @@ public class FirstPhase : MonoBehaviour
         attacking = false;
     }
 
-    void OnDrawGizmos()
+/*     void OnDrawGizmos()
     {
 
-    }
+    } */
 }
