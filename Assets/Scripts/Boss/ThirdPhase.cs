96fce00bfffd0c9d748fd164bdb249c220afc106
miloszkryzia1 <milosz.kryzia@gmail.com>
2023-05-03 22:00:35 -0700

    add boss third phase (spawning water monsters)

--- Assets/Scripts/Boss/ThirdPhase.cs
+++ Assets/Scripts/Boss/ThirdPhase.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ThirdPhase : MonoBehaviour
-{
-    // Start is called before the first frame update
+{    
+    public float timeUntilNextSpawn;
+    public GameObject waterMonster;
+    public Vector3 spawnLocation;
+    bool spawning = false;
+    Transform t;
+
     void Start()
     {
-        
+        t = gameObject.GetComponent<Transform>();   
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!spawning)
+        {
+            StartCoroutine(SpawnWaterMonster());
+        }
+    }
+
+    IEnumerator SpawnWaterMonster()
+    {
+        spawning = true;
+        yield return new WaitForSeconds(timeUntilNextSpawn);
+        Instantiate(waterMonster, spawnLocation, Quaternion.Euler(new Vector3(0,0,0)));
+        spawning = false;
     }
 }
